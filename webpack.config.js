94a899ea2f858501e74ff8275ae12bc9ea35310e
Chris Waldron <chriswaldron15@gmail.com>
2019-07-19 18:55:55 +0100

    Login screen, working JS build pipeline

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: './app/js/main.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
